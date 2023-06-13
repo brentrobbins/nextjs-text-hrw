7c15f5163d4a8e69837457aacb84fc13dc3ed977
Brent Robbins <brent@variantstudios.com>
2023-06-13 16:46:55 -0600

    Added revalidate 60 seconds

--- src/lib/getAllPosts.ts
+++ src/lib/getAllPosts.ts
@@ -1,5 +1,7 @@
 export default async function getAllPosts() {
-  const res = await fetch("https://api.tablebackend.com/v1/rows/1dQucBaX5vjr");
+  const res = await fetch("https://api.tablebackend.com/v1/rows/1dQucBaX5vjr", {
+    next: { revalidate: 10 },
+  });
   if (!res.ok) throw new Error(res.statusText);
   const posts = await res.json();
   return posts.data;
